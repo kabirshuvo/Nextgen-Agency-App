b481eba8ee892943801f415fa6d65a7e2d71aa7d
Kabir <kabirshuvo19@gmail.com>
2024-06-03 05:13:03 +0600

    Readme file updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 import ThemeToggler from "@/components/ThemeToggler";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Main Page</div>
-      <ThemeToggler />
+      <HeroSection />
     </main>
   );
 }
