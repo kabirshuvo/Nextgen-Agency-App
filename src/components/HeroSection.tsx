67f6a30d11082b3127197426903e049d37ac8c12
Kabir <kabirshuvo19@gmail.com>
2024-06-03 07:15:42 +0600

    Logo style Updated and hero text updated

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -14,10 +14,23 @@ import Socials from "./Socials";
 
 const HeroSection = () => {
   return (
-    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28">
+    <section className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
-          <div>text</div>
+          <div>
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
+              Brand Promotion
+            </div>
+            <h1 className="h1">
+              Graphic Design and <br /> Mobile First Web App Development
+            </h1>
+            <p>
+              Promotopea specializes in graphic design and mobile-friendly web
+              app development for effective brand promotion. From stunning
+              visuals to seamless user experiences, we help you connect with
+              your audience and elevate your online presence.
+            </p>
+          </div>
           <div>image</div>
         </div>
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,17 +1,31 @@
+"use client";
+import "../styles/hero.css";
+// Cannot find module '../styles/hero' or its corresponding type declarations.ts(2307)
+
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 const Logo = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <Link className="flex justify-center items-center gap-4" href="/">
+    <Link className="flex justify-center items-center gap-2" href="/">
       <Image
         src="/assets/images/logo.svg"
         width={32}
         height={32}
         priority
         alt="Promotopea Logo"
+        className={`${isHovered ? "rotate-360" : ""} ${
+          isHovered ? "glow" : ""
+        }`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       />
-      Promotopea
+      <span className="hover:text-blue-700 font-bold tracking-[2px]">
+        Promotopea
+      </span>
     </Link>
   );
 };
