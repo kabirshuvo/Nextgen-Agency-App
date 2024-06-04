23e42d8b0c90cc1b0b4eb46ddce135c962786cec
Kabir <kabirshuvo19@gmail.com>
2024-06-05 02:21:17 +0600

    delay added to h1 text

--- src/components/sections/HeroTexts.tsx
+++ src/components/sections/HeroTexts.tsx
@@ -12,23 +12,22 @@ const HeroTexts: React.FC = () => {
       </div>
       <div className="relative">
         <h1
-          className="h1 hover:text-popover transition duration-300 ease-in-out transform hover:scale-105 relative"
+          className="h1 hover:text-popover transition duration-300 ease-in-out transform relative delay-300"
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
           Graphic Design & Development
         </h1>
         <h1
-          className={`hiddenh1 absolute inset-0 flex justify-center items-center md:justify-start transition-opacity duration-300 ease-in-out ${
-            isHovered ? "opacity-100" : "opacity-0"
+          className={`hiddenh1 absolute inset-0 flex justify-center items-center md:justify-start transition-opacity duration-500 ease-in-out ${
+            isHovered ? "opacity-100 delay-200" : "opacity-0 delay-100"
           }`}
           style={{ pointerEvents: "none" }}
         >
           Creativity Is Our Key
         </h1>
-        {/* <RiEyeFillIcon /> */}
       </div>
-      <p className="subtitle hover:text-purple-700 transition duration-300 ease-in-out transform hover:scale-105">
+      <p className="subtitle hover:text-purple-700 transition duration-500 ease-in-out transform hover:scale-105">
         From stunning visuals to seamless user experiences, we help you connect
         with your audience and elevate your online presence.
       </p>
