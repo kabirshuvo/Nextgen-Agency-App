4b9a08216e6fddaf2a4cddfbc84ab1ddf56d4155
Kabir <kabirshuvo19@gmail.com>
2024-06-07 20:22:43 +0600

    primarily home page structure done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,14 @@ const Footer = () => {
   return (
     <footer className="bg-primary/20 py-16">
       <div className="container mx-auto">
-        <div>
-          <Socials />
+        <div className="flex flex-col items-center justify-between">
+          <Socials
+            containerStyles="flex gap-x-6 max-auto xl:mx-0 mb-4"
+            iconStyles="text-foreground text-[22px] hover:text-primary transition-all"
+          />
+          <div className="text-muted-foreground">
+            Copyright &copy; Promotopea. All rights reserved
+          </div>
         </div>
       </div>
     </footer>
