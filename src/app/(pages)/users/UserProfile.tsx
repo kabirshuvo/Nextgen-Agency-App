031ef2b7def89ec6253ccc804f07676bae3b850b
Kabir <kabirshuvo19@gmail.com>
2024-07-17 18:05:13 +0600

    error handle try 3

--- src/app/(pages)/users/UserProfile.tsx
+++ src/app/(pages)/users/UserProfile.tsx
@@ -1,23 +1,25 @@
+"use client";
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
-import { fetchUserData } from "../../../store/slices/userSlice";
+// import { fetchUserData } from "../../../store/slices/userSlice";
 
 const UserProfile: React.FC = () => {
   const dispatch = useAppDispatch();
-  const { data: user, status, error } = useAppSelector((state) => state.user);
+  // const { data: user, status, error } = useAppSelector((state) => state.user);
 
-  useEffect(() => {
-    dispatch(fetchUserData("someUserId"));
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchUserData("someUserId"));
+  // }, [dispatch]);
 
   if (status === "loading") return <div>Loading...</div>;
-  if (status === "failed") return <div>Error: {error}</div>;
-  if (!user) return null;
+  // if (status === "failed") return <div>Error: {error}</div>;
+  // if (!user) return null;
 
   return (
     <div>
-      <h1>{user.name}</h1>
-      <p>{user.email}</p>
+      user Profile testing
+      {/* <h1>{user.name}</h1>
+      <p>{user.email}</p> */}
     </div>
   );
 };
